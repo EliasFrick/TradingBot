336e0c471addb04edcd20d1c522170c0aae486c7
Elias Frick <112545876+EliasFrick@users.noreply.github.com>
2024-04-19 11:30:33 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,5 @@
 export var coinbaseConfig = {
-    API_KEY: 'AUjeJu8BbGALqK2V',
-    API_SECRET : 'SZDg43KiHRxLZnI7dW6frDhxkjjgcF2l',
 }
 
 export var polygonConfig = {
-    API_KEY: 'aME_VaReg47PM6BrMFDscM23CoqSurNb',
-    KEY_ID: 'a34098d9-41fe-4154-a313-d53351d8ec82'
 }
